c4f058adef9c2f4b20f69e25749763e376495c87
Alex Iglesias <alexiglesias@finsweet.com>
2022-08-26 16:05:08 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ import { greetUser } from '$utils/greet';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Erik Mejia';
+  const name = 'Erik Mejia!';
   greetUser(name);
   document.body.style.backgroundColor = 'red';
 });
