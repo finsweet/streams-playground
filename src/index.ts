e59f09d33dfb8eaa2a91dda75a121cc8294c67d9
Alex Iglesias <alexiglesias@finsweet.com>
2022-09-23 16:37:46 +0200

    stream updates :)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import { greetUser } from '$utils/greet';
+import {
+  cloneNode,
+  getCurrentBreakpoint,
+  getPublishDate,
+  getSiteId,
+  Interaction,
+  restartWebflow,
+  simulateEvent,
+  TabLinkElement,
+  TABS_CSS_CLASSES,
+} from '@finsweet/ts-utils';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'Erik Mejia!';
-  greetUser(name);
-  document.body.style.backgroundColor = 'red';
+  const tabLinks = document.querySelectorAll<TabLinkElement>(`.${TABS_CSS_CLASSES.tabLink}`);
+
+  simulateEvent(tabLinks[2], 'click');
+
+  const interaction = new Interaction({ element: tabLinks[2], duration: 500 });
+
+  interaction.trigger('first');
+
+  interaction.untilFinished()?.then(() => {
+    console.log('0interaction finished!');
+  });
+
+  console.log({ tabLinks });
 });
