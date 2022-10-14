3c851e29980ad892113d3d6551929d8b3b0997dc
Alex Iglesias <alexiglesias@finsweet.com>
2022-10-14 16:14:44 +0200

    stream - calculator code

--- packages/calculator/src/index.ts
+++ packages/calculator/src/index.ts
@@ -1,4 +1,38 @@
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  //
+  // Query the elements
+  const form = document.querySelector<HTMLFormElement>('[fs-element="form"]');
+  const resultLoan = document.querySelector<HTMLFormElement>('[fs-element="result-loan"]');
+  const resultInterest = document.querySelector<HTMLFormElement>('[fs-element="result-interest"]');
+  const resultTotal = document.querySelector<HTMLFormElement>('[fs-element="result-total"]');
+  const resultMonthly = document.querySelector<HTMLFormElement>('[fs-element="result-monthly"]');
+  if (!form || !resultLoan || !resultInterest || !resultTotal || !resultMonthly) return;
+
+  // Listen for form submission events
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Get the data from the calculator
+    const formData = new FormData(form);
+    const amount = formData.get('amount');
+    const interest = formData.get('interest');
+    const term = formData.get('term');
+
+    if (!amount || !interest || !term) return;
+
+    // Perform any maths
+    // I = P * r * T
+    const totalInterest = Number(amount) * (Number(interest) / 100) * Number(term);
+
+    const total = Number(amount) + totalInterest;
+    const monthlyPayment = total / 12;
+
+    // Display the results
+
+    resultLoan.textContent = amount.toString();
+    resultInterest.textContent = totalInterest.toString();
+    resultTotal.textContent = total.toString();
+    resultMonthly.textContent = monthlyPayment.toString();
+  });
 });
