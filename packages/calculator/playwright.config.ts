58e2ff0dce7cb39a5f1668dfbd4f78b2961fdfd0
Emily F <1789807+emilyf@users.noreply.github.com>
2022-10-18 07:55:21 -0600

    Include Original Calculator
    Added back in original calculator as well as the sliding scale calculator I started that includes the currency conversion piece we didn't quite finish from the live stream. Note: currency conversion is in slide-scale-calculator package index.ts for reference.

---
+++ packages/calculator/playwright.config.ts
@@ -0,0 +1,3 @@
+import config from '../../playwright.config';
+
+export default config;
---
+++ packages/calculator/src/index.ts
@@ -0,0 +1,37 @@
+window.Webflow ||= [];
+window.Webflow.push(() => {
+  // Query the elements
+  const form = document.querySelector<HTMLFormElement>('[fs-element="form"]');
+  const resultLoan = document.querySelector('[fs-element="result-loan"]');
+  const resultInterest = document.querySelector('[fs-element="result-interest"]');
+  const resultTotal = document.querySelector('[fs-element="result-total"]');
+  const resultMonthly = document.querySelector('[fs-element="result-monthly"]');
+
+  if (!form || !resultLoan || !resultInterest || !resultTotal || !resultMonthly) return;
+
+  // Listen for form submission events
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+
+    // Get the data
+    const formData = new FormData(form);
+    const amount = formData.get('amount');
+    const interest = formData.get('interest');
+    const term = formData.get('term');
+
+    if (!amount || !interest || !term) return;
+
+    // Perform calculations
+    // I = P * r * T
+    const totalInterest = Number(amount) * (Number(interest) / 100) * Number(term);
+    const total = Number(amount) + totalInterest;
+    const monthlyPayment = total / 12;
+
+    // Display the results
+    resultLoan.textContent = amount.toString();
+    resultInterest.textContent = totalInterest.toFixed(2);
+    resultTotal.textContent = total.toFixed(2);
+    resultMonthly.textContent = monthlyPayment.toFixed(2);
+  });
+});
